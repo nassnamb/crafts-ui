e88976813df3a6051c7ebb0de4e9b19ea5db0d76
loucény conde <louceny.conde@lne.fr>
2024-02-28 15:54:35 +0100

    Ajout des éléments dans App.css

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
 
     const [categoryList, setCategoryList] = useState([
         {"id":"1","job":"Cours particuliers","picture":"https://media.istockphoto.com/id/1034464980/fr/photo/m%C3%A8re-aider-sa-fille-%C3%A0-faire-ses-devoirs.jpg?s=2048x2048&w=is&k=20&c=q5-kDZHkPatNJzWpOi6GTc2Ik52hlmzdJoAhp8FkKzM="},
-        {"id":"2","job":"Aide à domicile","picture":"https://img.freepik.com/vecteurs-libre/boite-outils-pour-reparation-maison-bricolage_1284-6316.jpg?w=1380&t=st=1707687860~exp=1707688460~hmac=8154dd030e03dcf230273e6adb30aa6d20e9213d4e71c0d963f744612f3c79fb"},
+        {"id":"2","job":"Aide à domicile","picture":"https://st2.depositphotos.com/13193658/47011/i/450/depositphotos_470113266-stock-photo-smiling-social-worker-hugging-shoulder.jpg"},
         {"id":"3","job":"Informatique","picture":"https://www.nowteam.net/wp-content/uploads/2016/12/productivit%C3%A9-en-entreprise-1080x675.jpg"},
-        {"id":"4","job":"Enfants","picture":"https://img.freepik.com/vecteurs-libre/boite-outils-pour-reparation-maison-bricolage_1284-6316.jpg?w=1380&t=st=1707687860~exp=1707688460~hmac=8154dd030e03dcf230273e6adb30aa6d20e9213d4e71c0d963f744612f3c79fb"},
+        {"id":"4","job":"Enfants","picture":"https://www.shutterstock.com/shutterstock/photos/2172069263/display_1500/stock-photo-bunch-of-cheerful-joyful-cute-little-children-playing-together-and-having-fun-group-portrait-of-2172069263.jpg"},
         {"id":"5","job":"Santé","picture":"https://www.malakoffhumanis.com/sites/smile/files/styles/editorial_page_top_image_desktop/public/images/2021-162-mh-photo-centre-de-sante-missions-fonctionnement-gi-1221713778-1024x577.jpg?itok=r6-7_e4D"},
         {"id":"6","job":"Livraison","picture":"https://akanea.com/wp-content/uploads/2021/06/Illustration-livraison-urbaine-e-commerce.png"},
         {"id":"4","job":"Sports","picture":"https://www.consoglobe.com/wp-content/uploads/2017/08/deux-sports.jpg.webp"},
@@ -23,21 +23,14 @@ const Home = () => {
         {"id":"6","job":"Logistique transport","picture":"https://kmcl.fr/sites/default/files/styles/header_offre/public/medias/mon-metier-mes-usages-transport-et-logistique-3.jpg?itok=ux7X7vSh"},
     ]);
 
-    const CardsContainer = styled.div`
-      display: grid;
-      gap: 24px;
-      grid-template-rows: 350px 350px;
-      grid-template-columns: repeat(2, 1fr);
-      align-items: center;
-      justify-items: center;
-`
+
 
     return (
         <div>
             <AppNavbar /><br/>
             <Container fluid>
-                <div className="float-right">
-                    <CardsContainer>
+                <div>
+                    <div className="CardsContainer">
                         {categoryList.map((cat, index) => (
                             <Category
                                 key={`${cat.job}-${index}`}
@@ -45,7 +38,7 @@ const Home = () => {
                                 picture={cat.picture}
                             />
                         ))}
-                    </CardsContainer>
+                    </div>
                 </div>
             </Container>
         </div>
