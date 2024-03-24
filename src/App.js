dc94e436fe9aa78870fb46e7f2c0295c76ce6118
nassnamb <77997834+nassnamb@users.noreply.github.com>
2024-03-24 10:48:51 +0100

    Add new components for categories

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ import UserListPage from './UserListPage';
 import UserDetails from './UserDetails';
 import UserEdit from './UserEdit';
 import CoursParticuliers from './CoursParticuliers';
+import logistiqueTransport from "./LogistiqueTransport";
+import Sante from "./Sante";
+import Livraison from "./Livraison";
+import AideDomicile from "./AideDomicile";
+import Informatique from "./Informatique";
+import Sports from "./Sports";
+import Recrutement from "./Recrutement";
+import Evenementiel from "./Evenementiel";
+import Construction from "./Construction";
+import Beaute from "./Beaute";
+import Nettoyage from "./Nettoyage";
 
 function App() {
   return (
@@ -25,6 +36,18 @@ function App() {
             <Route path='/profiles/:id' component={ProfileEdit}/>
             <Route path='/users/:id' exact={true} component={UserEdit}/>
             <Route path='/cours-particuliers' exact={true} component={CoursParticuliers}/>
+            <Route path='/aide-domicile' exact={true} component={AideDomicile}/>
+            <Route path='/informatique' exact={true} component={Informatique}/>
+            <Route path='/sante' exact={true} component={Sante}/>
+            <Route path='/livraison' exact={true} component={Livraison}/>
+            <Route path='/sports' exact={true} component={Sports}/>
+            <Route path='/recrutement' exact={true} component={Recrutement}/>
+            <Route path='/evenementiel' exact={true} component={Evenementiel}/>
+            <Route path='/construction' exact={true} component={Construction}/>
+            <Route path='/beaute' exact={true} component={Beaute}/>
+            <Route path='/nettoyage' exact={true} component={Nettoyage}/>
+            <Route path='/aide-domicile' exact={true} component={AideDomicile}/>
+            <Route path='/logistique-transport' exact={true} component={logistiqueTransport}/>
             <Route render={() => <h1>404: page not found</h1>} />
           </Switch>
         </Router>
